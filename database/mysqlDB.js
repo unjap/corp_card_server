05435c81648dc31c856d9f99d48811c77868ab44
moonilson <und32g2ound@gmail.com>
2020-12-08 12:21:15 +0900

    [moonilson] 재연결 코드 원복

--- database/mysqlDB.js
+++ database/mysqlDB.js
@@ -1,34 +1,17 @@
 const mysql = require('mysql');
-let connection = null;
 
-const db_config = {
+let reConnection = null;
+const connection = mysql.createConnection({
     host: '114.202.216.45',
     post: 3306,
     user: 'iptvteam',
     password: 'open1404',
     database: 'corp',
     dateStrings: 'date'
-};
+});
 
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config);
-    connection.connect(function(err) {
-        if(err) {
-            console.log('error when connection to db: ', err);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-
-    connection.on('error', function(err) {
-        console.log('db error ', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    })
-}
-
-handleDisconnect()
+connection.handleDisconnect = () => {
+    reConnection = mysql.createConnection(connection);
+  }
 
 module.exports = connection;
