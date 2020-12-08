e255bd060ba97d0e90fdd8edaa6746730a3e192c
moonilson <und32g2ound@gmail.com>
2020-12-08 10:33:59 +0900

    [moonilson] db연결 로직 수정

--- database/mysqlDB.js
+++ database/mysqlDB.js
@@ -1,26 +1,27 @@
 const mysql = require('mysql');
+let connection = null;
 
-const db = mysql.createConnection({
+const db_config = {
     host: '114.202.216.45',
     post: 3306,
     user: 'iptvteam',
     password: 'open1404',
     database: 'corp',
     dateStrings: 'date'
-});
+};
 
 function handleDisconnect() {
-    db.connect(function(err) {
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {
         if(err) {
             console.log('error when connection to db: ', err);
             setTimeout(handleDisconnect, 2000);
         }
     });
 
-    db.on('error', function(err) {
+    connection.on('error', function(err) {
         console.log('db error ', err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            db.end();
             return handleDisconnect();
         } else {
             throw err;
@@ -30,4 +31,4 @@ function handleDisconnect() {
 
 handleDisconnect()
 
-module.exports = db;
+module.exports = connection;
